27c408ef3c878c71824becbc9d30439e2259917d
Aditya Amatya <63241408+amatya-aditya@users.noreply.github.com>
2023-11-05 01:17:04 +0545

    initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,142 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+const regexWiki = /\[\[([^\]]+)\]\]/
+const regexParenthesis = /\((.*?)\)/
+const regexWikiGlobal = /\[\[([^\]]*)\]\]/g
+const regexMdGlobal = /\[([^\]]*)\]\(([^\(]*)\)/g
 
-interface MyPluginSettings {
-	mySetting: string;
+const imageNumber: Record<string, number> = {
+	'a': 1,
+	'b': 2,
+	'c': 3,
+	'd': 4,
+	'e': 5,
+	'f': 6,
+	'g': 7,
+	'h': 8,
+	'i': 9,
+	'j': 10,
+	'k': 11,
+	'l': 12
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const addImageFromLink = (link: string, sourcePath: string, parent: HTMLElement, plugin: Plugin) => {
+	let destFile = app.metadataCache.getFirstLinkpathDest(link, sourcePath);
+	if (destFile) {
+		const img = parent.createEl("img");
+		img.src = plugin.app.vault.adapter.getResourcePath(destFile.path);
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const addExternalImage = (link: string, parent: HTMLElement) => {
+	const img = parent.createEl("img");
+	img.src = link;
+}
 
-	async onload() {
-		await this.loadSettings();
+const addPlaceHolder = (widthXHeight: string, parent: HTMLElement) => {
+	widthXHeight = widthXHeight ?? "640x480";
+	const img = parent.createEl("img");
+	img.src = `https://via.placeholder.com/${widthXHeight}`;
+}
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+const renderLayout = (
+	images: ImageLink[],
+	slides: string,
+	sourcePath: string,
+	parent: HTMLElement,
+	plugin: Plugin) => {
+	
+	// add placeholders if images are missing
+	const imageNumberCount = imageNumber[slides];
+	if (images.length < imageNumberCount) {
+		for (let i = images.length; i < imageNumberCount; i++) {
+			images.push({ type: 'placeholder' });
+		}
+	}
+	if (images.length > imageNumberCount) {
+		images = images.slice(0, imageNumberCount);
+	}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	const div = parent.createEl("div", { cls: `image-layouts-grid image-layouts-layout-${slides}` });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+	images.forEach((image, idx) => {
+		const imgdiv = div.createEl("div", { cls: `image-layouts-image` });
+		if (image.type === 'local') {
+			addImageFromLink(image.link, sourcePath, imgdiv, plugin);
+		} else if (image.type === 'external') {
+			console.log(image.link);
+			addExternalImage(image.link, imgdiv);
+		} else if (image.type === 'placeholder') {
+			addPlaceHolder('640x480', imgdiv);
+		}
+	});
+
+
+	// create next and previous slide button
+
+	function nextSlide(){
+		
+	}
+
+	function prevSlide(){
+		
+
+	}
+
+	// create next and previous button html element
+	const buttonDiv = parent.createEl("div", {cls: "slide-controls"});
+	let prevButtonEl = buttonDiv.createEl("button", { text: "Prev", cls: "prev-img-btn" });
+	let nextButtonEl = buttonDiv.createEl("button", { text: "Next", cls: "next-img-btn" });
+
+
+	nextButtonEl.addEventListener("click", () => {
+		nextSlide();
+	});
+
+	prevButtonEl.addEventListener("click", () => {
+		prevSlide();
+	});
+
+}
+
+const getImages = (source: string): ImageLink[] => {
+	const lines = source.split('\n').filter((row) => row.startsWith('!'));
+	const images = lines.map((line) => getImageFromLine(line));
+	return images.filter((image) => image !== null) as ImageLink[];
+}
+
+type ImageLink = {
+	type: 'local' | 'external';
+	link: string;
+} | {
+	type: 'placeholder';
+}
+
+const getImageFromLine = (line: string): ImageLink | null => {
+	if (line.match(regexMdGlobal)) {
+		const link = line.match(regexParenthesis)?.[1];
+		if (link) {
+			return { type: 'external', link };
+		}
+	} else if (line.match(regexWikiGlobal)) {
+		const link = line.match(regexWiki)?.[1];
+		if (link) {
+			return {
+				type: 'local',
+				link: link
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		}
 	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	return null;
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+export default class ImageNumberPlugin extends Plugin {
+  async onload() {
+	Object.keys(imageNumber).forEach((slides) => {
+		this.registerMarkdownCodeBlockProcessor(`image-slider-${slides}`, (source, el, ctx) => {
+			const images = getImages(source);
+			renderLayout(images, slides, ctx.sourcePath, el, this);
+		});
+	});
+  }
 }
